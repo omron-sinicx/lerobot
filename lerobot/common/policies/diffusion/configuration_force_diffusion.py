7bb9c72e1ba422aa229be34bf89893b14c3f398f
malek <mk.aburrub@gmail.com>
2024-08-19 04:10:24 +0000

    modify to accept multiple outputs

--- lerobot/common/policies/diffusion/configuration_force_diffusion.py
+++ lerobot/common/policies/diffusion/configuration_force_diffusion.py
@@ -129,6 +129,7 @@ class ForceDiffusionConfig:
     # Vision backbone.
     vision_backbone: str = "resnet18"
     crop_shape: tuple[int, int] | None = (84, 84)
+    # crop_shape: tuple[int, int] | None = (420, 420)
     crop_is_random: bool = True
     pretrained_backbone_weights: str | None = None
     use_group_norm: bool = True
