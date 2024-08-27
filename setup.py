aa3f51799973e88e760b7f4160054fdb40221314
Cristian Beltran <cristianbehe@gmail.com>
2024-08-27 02:32:54 +0000

    Make lerobot installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lerobot",
+    version="0.1.0",
+    description="lerobot",
+    url="https://github.com/huggingface/lerobot",
+    author="huggingface",
+    license="Apache License 2.0",
+    packages=find_packages("."),
+)
